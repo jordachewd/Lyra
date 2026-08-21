ec6ba05db3178b3e95940a4c1dd606b1b7ba2b5c
FlorinIO <florin.iordache@jordachewd.ro>
2026-08-21 14:21:13 +0300

    chore: update package.json and sanity.config.ts to include new plugins and dependencies
    - Modified the deploy script to build before deploying.
    - Added new dependencies: @sanity/code-input, @sanity/color-input, @sanity/icons, @sanity/table, and sanity-plugin-media.
    - Updated sanity.config.ts to include the new plugins: codeInput, colorInput, table, and media.

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,6 +1,10 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {codeInput} from '@sanity/code-input'
+import {colorInput} from '@sanity/color-input'
+import {table} from '@sanity/table'
+import {media} from 'sanity-plugin-media'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
@@ -10,7 +14,7 @@ export default defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_DATASET!,
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), media(), codeInput(), colorInput(), table(), visionTool()],
 
   schema: {
     types: schemaTypes,
